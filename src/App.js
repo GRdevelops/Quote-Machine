f10814118f772f5c2ec2711aaca1e9c26a6c6c48
GRdevelops <gianniruggieri99@gmail.com>
2023-10-24 19:06:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [color, setColor] = useState('');
+	const colors = ['blue', 'red', 'green'];
+
+	const handleClick = (event) => {
+		let newColor;
+
+		do {
+			const randomIndex = Math.floor(Math.random() * colors.length);
+			newColor = colors[randomIndex];
+		} while (newColor === color);
+
+		setColor(newColor);
+	};
+
+	return (
+		<main style={{ backgroundColor: color }}>
+			<section>
+				<div className='container'>
+					<div className='quote' style={{ color: color }}>
+						<FontAwesomeIcon icon={faQuoteLeft} className='left-quote' style={{ color: color }} />
+						Few things can help an individual more than to place responsibility on him, and to let him know that you
+						trust him.
+					</div>
+					<span className='author' style={{ color: color }}>
+						- Earl Nightingale
+					</span>
+					<div className='ctas'>
+						<div>
+							<a>
+								<FontAwesomeIcon icon={faTwitter} className='icon' style={{ backgroundColor: color }} />
+							</a>
+							<a>
+								<FontAwesomeIcon icon={faPaperPlane} className='icon' style={{ backgroundColor: color }} />
+							</a>
+						</div>
+						<button onClick={handleClick} style={{ backgroundColor: color }}>
+							New quote
+						</button>
+					</div>
+				</div>
+				<div className='me'>
+					<span>
+						by <a>giovanni</a>
+					</span>
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
